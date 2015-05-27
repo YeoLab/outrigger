d6e75ec80e238341edfede741971d59bddb4d079
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-05-27 16:16:50 -0700

    Add initial tests for reading hmmscan output

---
+++ poshsplice/hmmscan.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+import pandas as pd
+
+def read_hmmscan(hmmscan_out):
+    """Read output from hmmscan
+
+    Parameters
+    ----------
+    hmmscan_out : str
+        Filename of hmmscan output
+
+    Returns
+    -------
+    hmmscan_df : pandas.DataFrame
+        Parsed string of the hmmscan output
+    """
+    entries = []
+    with open(hmmscan_out) as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            split = line.split()
+            beginning = split[:22]
+            end = ' '.join(split[22:])
+            entries.append(beginning + [end])
+    columns = ['target_name', 'target_accession', 'target_length', 'query_name', 'query_accession', 'query_length', 'sequence_e_value',
+             'sequence_score', 'sequence_bias', 'domain_number', 'domain_total', 'domain_conditional_e_value', 'domain_independent_e_value',
+             'domain_score', 'domain_bias', 'target_start', 'target_stop', 'query_start', 'query_stop', 'query_domain_envelope_start',
+             'query_domain_envelope_stop', 'mean_posterior_probability', 'target_description']
+    df = pd.DataFrame.from_records(entries, columns=columns)
+    df = df.convert_objects(convert_numeric=True)
+    return df
---
+++ tests/test_hmmscan.py
@@ -0,0 +1,46 @@
+try:
+    # For Python 2
+    from StringIO import StringIO
+except ImportError:
+    # For Python 3
+    from io import StringIO
+
+import pandas as pd
+import pandas.util.testing as pdt
+import pytest
+
+@pytest.fixture
+def example_hmmscan_out(tmpdir):
+    s = '''#                                                                             --- full sequence --- -------------- this domain -------------   hmm coord   ali coord   env coord
+# target name        accession   tlen query name            accession   qlen   E-value  score  bias   #  of  c-Evalue  i-Evalue  score  bias  from    to  from    to  from    to  acc description of target
+#------------------- ---------- -----  -------------------- ---------- ----- --------- ------ ----- --- --- --------- --------- ------ ----- ----- ----- ----- ----- ----- ----- ---- ---------------------
+Fox-1_C              PF12414.3     93 sp|O43251|RFOX2_HUMAN -            390   3.2e-39  133.2  29.5   1   2      0.23   6.7e+02    0.7   0.0    14    48   177   213   166   243 0.66 Calcitonin gene-related peptide regulator C terminal
+Fox-1_C              PF12414.3     93 sp|O43251|RFOX2_HUMAN -            390   3.2e-39  133.2  29.5   2   2   8.9e-42   2.6e-38  130.2  27.3     2    93   265   362   264   362 0.97 Calcitonin gene-related peptide regulator C terminal
+RRM_1                PF00076.17    70 sp|O43251|RFOX2_HUMAN -            390     8e-19   67.0   0.1   1   1   5.9e-22   1.7e-18   65.9   0.1     2    70   124   191   123   191 0.97 RNA recognition motif. (a.k.a. RRM, RBD, or RNP domain)
+RRM_6                PF14259.1     70 sp|O43251|RFOX2_HUMAN -            390   2.4e-15   56.2   0.1   1   1   1.4e-18   4.3e-15   55.4   0.1     1    70   123   191   123   191 0.95 RNA recognition motif (a.k.a. RRM, RBD, or RNP domain)
+RRM_5                PF13893.1     56 sp|O43251|RFOX2_HUMAN -            390   8.1e-11   41.6   0.1   1   1   5.9e-14   1.8e-10   40.5   0.1     1    54   137   193   137   195 0.90 RNA recognition motif. (a.k.a. RRM, RBD, or RNP domain)
+RRM_3                PF08777.6    105 sp|O43251|RFOX2_HUMAN -            390     0.084   12.7   0.0   1   1   6.7e-05       0.2   11.5   0.0    17    79   136   202   127   206 0.83 RNA binding motif
+#
+# Program:         hmmscan
+# Version:         3.1b1 (May 2013)
+# Pipeline mode:   SCAN
+# Query file:      /projects/ps-yeolab/genomes/pfam/release_27/ftp.sanger.ac.uk/pub/databases/Pfam/releases/Pfam27.0/RBFOX2_human.fasta
+# Target file:     /projects/ps-yeolab/genomes/pfam/release_27/ftp.sanger.ac.uk/pub/databases/Pfam/releases/Pfam27.0/Pfam-A.hmm
+# Option settings: hmmscan --domtblout RBFOX2_human_pfam.txt --noali --notextw /projects/ps-yeolab/genomes/pfam/release_27/ftp.sanger.ac.uk/pub/databases/Pfam/releases/Pfam27.0/Pfam-A.hmm /projects/ps-yeolab/genomes/pfam/release_27/ftp.sanger.ac.uk/pub/databases/Pfam/releases/Pfam27.0/RBFOX2_human.fasta
+# Current dir:     /home/obotvinnik/ipython_notebook/miso
+# Date:            Tue Jan 27 18:56:23 2015
+# [ok]
+'''
+    filename = '{}/hmmscan_out.txt'.format(tmpdir)
+    with open(filename, 'w') as f:
+        f.write(s)
+    return filename
+
+
+def test_read_hmmscan_out(example_hmmscan_out):
+    from poshsplice.hmmscan import read_hmmscan
+
+    test = read_hmmscan(example_hmmscan_out)
+    true_s = ',target_name,target_accession,target_length,query_name,query_accession,query_length,sequence_e_value,sequence_score,sequence_bias,domain_number,domain_total,domain_conditional_e_value,domain_independent_e_value,domain_score,domain_bias,target_start,target_stop,query_start,query_stop,query_domain_envelope_start,query_domain_envelope_stop,mean_posterior_probability,target_description\n0,Fox-1_C,PF12414.3,93,sp|O43251|RFOX2_HUMAN,-,390,3.2e-39,133.2,29.5,1,2,0.23,6.7e+02,0.7,0.0,14,48,177,213,166,243,0.66,Calcitonin gene-related peptide regulator C terminal\n1,Fox-1_C,PF12414.3,93,sp|O43251|RFOX2_HUMAN,-,390,3.2e-39,133.2,29.5,2,2,8.9e-42,2.6e-38,130.2,27.3,2,93,265,362,264,362,0.97,Calcitonin gene-related peptide regulator C terminal\n2,RRM_1,PF00076.17,70,sp|O43251|RFOX2_HUMAN,-,390,8e-19,67.0,0.1,1,1,5.9e-22,1.7e-18,65.9,0.1,2,70,124,191,123,191,0.97,"RNA recognition motif. (a.k.a. RRM, RBD, or RNP domain)"\n3,RRM_6,PF14259.1,70,sp|O43251|RFOX2_HUMAN,-,390,2.4e-15,56.2,0.1,1,1,1.4e-18,4.3e-15,55.4,0.1,1,70,123,191,123,191,0.95,"RNA recognition motif (a.k.a. RRM, RBD, or RNP domain)"\n4,RRM_5,PF13893.1,56,sp|O43251|RFOX2_HUMAN,-,390,8.1e-11,41.6,0.1,1,1,5.9e-14,1.8e-10,40.5,0.1,1,54,137,193,137,195,0.90,"RNA recognition motif. (a.k.a. RRM, RBD, or RNP domain)"\n5,RRM_3,PF08777.6,105,sp|O43251|RFOX2_HUMAN,-,390,0.084,12.7,0.0,1,1,6.7e-05,0.2,11.5,0.0,17,79,136,202,127,206,0.83,RNA binding motif\n'
+    true = pd.read_csv(StringIO(test.to_csv()), index_col=0)
+    pdt.assert_frame_equal(test, true)
