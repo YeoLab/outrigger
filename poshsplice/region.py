b8fb9824353e0810e6ff9ab31b9de77dd9aab507
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-09-09 09:20:28 -0700

    make start, stop, name a property

--- poshsplice/region.py
+++ poshsplice/region.py
@@ -1,5 +1,7 @@
 """Define locations in the genome"""
 
+import six
+
 class Region(object):
 
     __slots__ = ('name', 'region', 'chrom', 'start', 'stop', '_start',
@@ -28,28 +30,43 @@ class Region(object):
                              ' ({1})'.format(start, stop))
 
         self.region = region
-        self.chrom = chrom
+        self.chrom = six.u(chrom)
 
         # Use "private" variables to store the true genome location
         self._start = start
         self._stop = stop
-        self.strand = strand
+        self.strand = six.u(strand)
 
-        # Use non-private start, stop to save negative strand coordinates with
-        # negative numbers to make the calculation of "before" and "after" in
-        # the transcript easier
+    # Use non-private start, stop to save negative strand coordinates with
+    # negative numbers to make the calculation of "before" and "after" in
+    # the transcript easier
+    @property
+    def start(self):
         if self.strand == '-':
-            self.start = -start
-            self.stop = -stop
+            return -self._start
         else:
-            self.start = start
-            self.stop = stop
+            return self._start
+
+    @property
+    def stop(self):
+        if self.strand == '-':
+            return -self._stop
+        else:
+            return self._stop
+
+    @property
+    def name(self):
+        base = '{0}:{1}-{2}:{3}'.format(self.chrom, self._start,
+                                        self._stop, self.strand)
+        if self.region is not None:
+            base = self.region + ':' + base
+        return six.u(base)
 
     def __repr__(self):
         return 'poshsplice.Region <{0}>'.format(self.name)
 
     def __str__(self):
-        return self.name
+        return six.u(self.name)
 
     def __eq__(self, other):
         return all(getattr(self, attr) == getattr(other, attr)
