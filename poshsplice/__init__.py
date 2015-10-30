686999bb8167b9da72beae050ac7f73166766f39
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-10-30 08:04:10 -0700

    add wannabe

--- poshsplice/__init__.py
+++ poshsplice/__init__.py
@@ -3,3 +3,11 @@
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
+
+
+def wannabe():
+    try:
+        from IPython.display import YouTubeVideo
+        YouTubeVideo("8X-2czaa3WA", autoplay=1, start=4)
+    except:
+        pass
