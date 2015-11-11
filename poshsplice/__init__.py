5ba4af36799dfc05b030ca1524978e02e8d9320d
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-11-10 17:15:05 -0800

    fix up wannabe to actually play when called

--- poshsplice/__init__.py
+++ poshsplice/__init__.py
@@ -7,7 +7,8 @@ __version__ = '0.1.0'
 
 def wannabe():
     try:
-        from IPython.display import YouTubeVideo
-        YouTubeVideo("8X-2czaa3WA", autoplay=1, start=4)
-    except:
+        from IPython.display import YouTubeVideo, display
+        vid = YouTubeVideo("8X-2czaa3WA", autoplay=1, start=4)
+        display(vid)
+    except ImportError:
         pass
