a3012431244a1ac55f0b1a0c0aaaed0193c1b252
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-01-20 12:04:46 -0800

    trying to rewrite argparse to use subcommands properly

--- outrigger/outrigger.py
+++ outrigger/outrigger.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 import datetime
 import os
@@ -18,7 +20,17 @@ class CommandLine(object):
         else:
             self.args = vars(self.parser.parse_args(inOpts))
 
-    def build(self):
+        # # parse_args defaults to [1:] for args, but you need to
+        # # exclude the rest of the args too, or validation will fail
+        # args = self.parser.parse_args(sys.argv[1:2])
+        # if not hasattr(self, args.command):
+        #     sys.stderr.write('Unrecognized command\n')
+        #     self.parser.print_help()
+        #     exit(1)
+        # # use dispatch pattern to invoke method with same name
+        # getattr(self, args.command)()
+
+    # def build(self):
         """Subcommand to build the index of splicing events"""
         build_parser = self.subparser.add_parser(
             'build', help='Build an index of splicing events using a graph '
@@ -51,22 +63,22 @@ class CommandLine(object):
                                  action='store', default='./outrigger_output',
                                  help='Where you want to save the generated files')
 
-    def psi(self):
+    # def psi(self):
         """Subcommand to calculate psi on the built index"""
         psi_parser = self.subparser.add_parser(
             'psi', help='Calculate "percent spliced-in" (Psi) values')
         psi_parser.add_argument('-j')
 
-    def do_usage_and_die(self, str):
-        '''
-        If a critical error is encountered, where it is suspected that the
-        program is not being called with consistent parameters or data, this
-        method will write out an error string (str), then terminate execution
-        of the program.
-        '''
-        sys.stderr.write(str)
-        self.parser.print_usage()
-        return 2
+    # def do_usage_and_die(self, str):
+    #     '''
+    #     If a critical error is encountered, where it is suspected that the
+    #     program is not being called with consistent parameters or data, this
+    #     method will write out an error string (str), then terminate execution
+    #     of the program.
+    #     '''
+    #     sys.stderr.write(str)
+    #     self.parser.print_usage()
+    #     return 2
 
 
 # Class: Usage
@@ -81,29 +93,29 @@ class Usage(Exception):
 
 
 if __name__ == '__main__':
-    try:
-        cl = CommandLine()
+    # try:
+    cl = CommandLine()
 
-        gtf = cl.args['gtf']
-        db_filename = gtf + '.db'
-        if gtf is not None:
-            if os.path.exists(db_filename):
-                db = gffutils.FeatureDB(db_filename)
-            else:
-                sys.stdout.write('Starting writing the gffutils FeatureDB of '
-                                 '{} ...\n\tStart:{}\n'.format(
-                    gtf, datetime.datetime.now()))
-                from .gtf import create_db
-                db = create_db(gtf, db_filename)
-                sys.stdout.write('\tDone:{}\n'.format(
-                    datetime.datetime.now()))
-        else:
-            db = gffutils.FeatureDB(cl.args['db'])
+        # gtf = cl.args['gtf']
+        # db_filename = gtf + '.db'
+        # if gtf is not None:
+        #     if os.path.exists(db_filename):
+        #         db = gffutils.FeatureDB(db_filename)
+        #     else:
+        #         sys.stdout.write('Starting writing the gffutils FeatureDB of '
+        #                          '{} ...\n\tStart:{}\n'.format(
+        #             gtf, datetime.datetime.now()))
+        #         from .gtf import create_db
+        #         db = create_db(gtf, db_filename)
+        #         sys.stdout.write('\tDone:{}\n'.format(
+        #             datetime.datetime.now()))
+        # else:
+        #     db = gffutils.FeatureDB(cl.args['db'])
+        #
+        # splice_junctions = pd.read_csv(cl.args['junctions'],
+        #                                sep=cl.args['sep'])
+        #
+        # ja = JunctionAnnotator(splice_junctions, db, **cl.args)
 
-        splice_junctions = pd.read_csv(cl.args['junctions'],
-                                       sep=cl.args['sep'])
-
-        ja = JunctionAnnotator(splice_junctions, db, **cl.args)
-
-    except Usage as err:
-        cl.do_usage_and_die()
+    # except Usage as err:
+    #     cl.do_usage_and_die()
