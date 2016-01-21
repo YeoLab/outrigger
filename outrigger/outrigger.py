f489a916dc4ea59c0143522dc007f87d9b0009dd
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-01-21 07:46:22 -0800

    Trying to simplify parsing code

--- outrigger/outrigger.py
+++ outrigger/outrigger.py
@@ -11,8 +11,9 @@ import gffutils
 class CommandLine(object):
     def __init__(self, inOpts=None):
         self.parser = argparse.ArgumentParser(
-            description='Calculate percent-spliced in on a *de novo* '
-                        'custom-built splicing index')
+            description='Calculate "percent-spliced in" (Psi) scores of '
+                        'alternative splicing on a *de novo*, custom-built '
+                        'splicing index')
         self.subparser = self.parser.add_subparsers(help='Sub-commands')
 
 
@@ -51,68 +52,44 @@ class CommandLine(object):
                                    "use. The exon IDs defined here will be used in "
                                    "the function")
         build_parser.add_argument('-o', '--output', required=False, type=str,
-                                 action='store', default='./outrigger_output',
-                                 help='Where you want to save the generated files')
+                                 action='store', default='./outrigger_index',
+                                 help='Where you want to save the index of '
+                                      'splicing events')
 
     # def psi(self):
         """Subcommand to calculate psi on the built index"""
         psi_parser = self.subparser.add_parser(
             'psi', help='Calculate "percent spliced-in" (Psi) values using the '
                         'splicing event index built with "outrigger index"')
-        psi_parser.add_argument('-j')
+        psi_parser.add_argument('-i', '--index', required=True,
+                                help='Name of the folder where you saved the '
+                                     'output from "outrigger index" (default '
+                                     'is ./outrigger_index from the directory '
+                                     'where you called the program)"')
+        psi_parser.add_argument('-j', '--splice-junction', required=False,
+                                help="Name of the splice junction files. If "
+                                     "not provided, the compiled 'sj.csv' file"
+                                     "that was created during 'outrigger "
+                                     "index' will be used")
 
         if inOpts is None:
             self.args = self.parser.print_usage()
         else:
             self.args = vars(self.parser.parse_args(inOpts))
 
-    # def do_usage_and_die(self, str):
-    #     '''
-    #     If a critical error is encountered, where it is suspected that the
-    #     program is not being called with consistent parameters or data, this
-    #     method will write out an error string (str), then terminate execution
-    #     of the program.
-    #     '''
-    #     sys.stderr.write(str)
-    #     self.parser.print_usage()
-    #     return 2
+    def do_usage_and_die(self, str):
+        '''
+        If a critical error is encountered, where it is suspected that the
+        program is not being called with consistent parameters or data, this
+        method will write out an error string (str), then terminate execution
+        of the program.
+        '''
+        sys.stderr.write(str)
+        self.parser.print_usage()
+        return 2
 
-
-# Class: Usage
-class Usage(Exception):
-    '''
-    Used to signal a Usage error, evoking a usage statement and eventual
-    exit when raised
-    '''
-
-    def __init__(self, msg):
-        self.msg = msg
 
 
 if __name__ == '__main__':
     # try:
     cl = CommandLine()
-
-        # gtf = cl.args['gtf']
-        # db_filename = gtf + '.db'
-        # if gtf is not None:
-        #     if os.path.exists(db_filename):
-        #         db = gffutils.FeatureDB(db_filename)
-        #     else:
-        #         sys.stdout.write('Starting writing the gffutils FeatureDB of '
-        #                          '{} ...\n\tStart:{}\n'.format(
-        #             gtf, datetime.datetime.now()))
-        #         from .gtf import create_db
-        #         db = create_db(gtf, db_filename)
-        #         sys.stdout.write('\tDone:{}\n'.format(
-        #             datetime.datetime.now()))
-        # else:
-        #     db = gffutils.FeatureDB(cl.args['db'])
-        #
-        # splice_junctions = pd.read_csv(cl.args['junctions'],
-        #                                sep=cl.args['sep'])
-        #
-        # ja = JunctionAnnotator(splice_junctions, db, **cl.args)
-
-    # except Usage as err:
-    #     cl.do_usage_and_die()
