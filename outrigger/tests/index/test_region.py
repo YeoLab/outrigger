aa8155a69ad0ebf049bfd14c2e64463d8fe1aff6
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-04-28 12:58:32 -0700

    Add working tests for region!!

---
+++ outrigger/tests/index/test_region.py
@@ -0,0 +1,119 @@
+import pytest
+
+@pytest.fixture(params=['-', '+'])
+def location(request):
+    return 'chr1:100-200:{}'.format(request.param)
+
+
+@pytest.fixture
+def location_with_region_name():
+    return 'junction:chr1:100-200:+'
+
+class TestRegion(object):
+
+    def test___init(self, location):
+        from outrigger.index.region import Region
+
+        r = Region(location)
+
+        assert r.region is None
+        assert r.chrom == 'chr1'
+        assert r.start == 100
+        assert r.stop == 200
+        assert r.strand == location[-1]
+
+    def test___init_region_name(self, location_with_region_name):
+        from outrigger.index.region import Region
+
+        r = Region(location_with_region_name)
+        assert r.region == 'junction'
+        assert r.chrom == 'chr1'
+        assert r.start == 100
+        assert r.stop == 200
+        assert r.strand == '+'
+
+    @pytest.mark.xfail
+    def test___init_start_larger_than_stop(self):
+        from outrigger.index.region import Region
+
+        Region('chr1:200-100:+')
+
+    def test__start(self, location):
+        from outrigger.index.region import Region
+
+        r = Region(location)
+
+        true__start = -r.start if r.strand == '-' else r.start
+        assert r._start == true__start
+
+    def test__stop(self, location):
+        from outrigger.index.region import Region
+
+        r = Region(location)
+
+        true__stop = -r.stop if r.strand == '-' else r.stop
+        assert r._stop == true__stop
+
+    def test___len(self, location):
+        from outrigger.index.region import Region
+
+        r = Region(location)
+
+        assert len(r) == 101
+
+    def test___str(self, location):
+        from outrigger.index.region import Region
+
+        r = Region(location)
+
+        assert r.name == location
+
+    def test___str_with_region_name(self, location_with_region_name):
+        from outrigger.index.region import Region
+
+        r = Region(location_with_region_name)
+
+        assert r.name == location_with_region_name
+
+    def test___eq(self, location_with_region_name):
+        from outrigger.index.region import Region
+
+        r1 = Region(location_with_region_name)
+        r2 = Region(location_with_region_name)
+
+        assert r1 == r2
+
+    def test___neq(self, location_with_region_name, location):
+        from outrigger.index.region import Region
+
+        r1 = Region(location_with_region_name)
+        r2 = Region(location)
+
+        assert r1 != r2
+
+    def test_overlaps_true(self, location_with_region_name, location):
+        from outrigger.index.region import Region
+
+        r1 = Region(location_with_region_name)
+        r2 = Region(location)
+
+        assert r1.overlaps(r2)
+        assert r2.overlaps(r1)
+
+    def test_overlaps_false_same_chrom(self, location):
+        from outrigger.index.region import Region
+
+        r1 = Region(location)
+        r2 = Region('chr1:400-500:-')
+
+        assert not r1.overlaps(r2)
+        assert not r2.overlaps(r1)
+
+    def test_overlaps_false_different_chrom(self, location):
+        from outrigger.index.region import Region
+
+        r1 = Region(location)
+        r2 = Region('chr2:400-500:-')
+
+        assert not r1.overlaps(r2)
+        assert not r2.overlaps(r1)
