f50686c3892cd94487237de7cebd6009264ddb42
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-04-28 12:13:54 -0700

    Add working database creation test

--- outrigger/tests/io/test_gtf.py
+++ outrigger/tests/io/test_gtf.py
@@ -1,6 +1,28 @@
-def test_create_db(gtf_filename):
+
+import gffutils
+import pytest
+
+def test_create_db(gtf_filename, db):
     from outrigger.io import gtf
 
-    db = gtf.create_db(gtf_filename)
+    true = db
 
-    assert False
+    test = gtf.create_db(gtf_filename)
+
+    # Check that all the true db features are in the test database
+    for featuretype in true.featuretypes():
+        for feature in true.features_of_type(featuretype=featuretype):
+            try:
+                test[feature.id]
+            except gffutils.FeatureNotFoundError:
+                pytest.fail('Feature in true database not found in test '
+                            'database')
+
+    # Check that all the test db features are in the true database
+    for featuretype in test.featuretypes():
+        for feature in test.features_of_type(featuretype=featuretype):
+            try:
+                true[feature.id]
+            except gffutils.FeatureNotFoundError:
+                pytest.fail('Feature in test database not found in true '
+                            'database')
