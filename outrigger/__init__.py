9c1f0f12f9544a05939797070ffa4ac1eacc02c1
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-08-22 13:00:39 -0700

    Change all export of __init__

--- outrigger/__init__.py
+++ outrigger/__init__.py
@@ -4,5 +4,4 @@ __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
-__all__ = ['psi', 'events', 'gtf', 'junctions', 'psi', 'region', 'star',
-           'util']
+__all__ = ['psi', 'region', 'util', 'io']
