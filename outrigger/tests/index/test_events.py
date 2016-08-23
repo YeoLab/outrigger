e5b8012d7bb2f9182bb0e9bc16e737859bcb5673
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-08-23 14:14:17 -0700

    typo in output assertion

--- outrigger/tests/index/test_events.py
+++ outrigger/tests/index/test_events.py
@@ -236,7 +236,7 @@ class TestEventMaker(object):
         test = getattr(event_maker, longname)()
         out, err = capsys.readouterr()
         assert 'Trying out' in out
-        assert 'exon_cols tested' in out
+        assert 'exons tested' in out
         assert '%' in out
 
         true = events.copy()
