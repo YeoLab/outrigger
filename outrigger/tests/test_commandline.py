1106f5cb61808a76527e4171cc46e6a0432630fb
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-09-13 09:48:22 -0700

    Start writing tests for commandline

--- outrigger/tests/test_commandline.py
+++ outrigger/tests/test_commandline.py
@@ -1,40 +1,68 @@
 
-# import filecmp
-# import glob
-# import os
+import filecmp
+import glob
+import os
 
 
 class TestSubcommand(object):
 
-    def test___init(self):
+    def test___init(self, tmpdir):
         from outrigger.commandline import Subcommand
 
-        kwargs = dict(asdf="beyonce", jkl=1234)
+        output = os.path.join(tmpdir.strpath, 'blue_ivy')
 
-        s = Subcommand(**kwargs)
+        kwargs = dict(asdf="beyonce", jkl=1234, output=output)
+
+        subcommand = Subcommand(**kwargs)
 
         for key, value in kwargs.items():
-            assert getattr(s, key) == value
+            assert getattr(subcommand, key) == value
+        for folder in subcommand.folders:
+            assert os.path.exists(folder)
 
 
-# def test_main_index(tmpdir):
-#     from outrigger.commandline import CommandLine
-#
-#     sj_out_tab = ' '.join(glob.iglob('outrigger/test_data/tasic2016/unprocessed/sj_out_tab/*'))  # noqa
-#     gtf = ' outrigger/test_data/tasic2016/unprocessed/gtf/gencode.vM10.annotation.snap25.myl6.gtf'  # noqa
-#     args = 'index --sj-out-tab {splice_junctions} --gtf {gtf} --output {output}'.format(  # noqa
-#         splice_junctions=sj_out_tab, gtf=gtf, output=tmpdir.strpath).split()
-#     # import pdb; pdb.set_trace()
-#     CommandLine(args)
-#
-#     dir1 = os.path.join(tmpdir.strpath, 'index')
-#     dir2 = os.path.join('outrigger', 'test_data', 'tasic2016',
-#                         'outrigger_output', 'index')
-#     directory_comparison = filecmp.dircmp(dir1, dir2,
-#                                           ignore=['psi', '.DS_Store'])
-#     assert len(directory_comparison.left_only) == 0
-#     assert len(directory_comparison.right_only) == 0
-#
+def test_main_index(tmpdir):
+    from outrigger.commandline import CommandLine
+
+    sj_out_tab = ' '.join(glob.iglob('outrigger/tests/data/tasic2016/unprocessed/sj_out_tab/*'))  # noqa
+    gtf = ' outrigger/tests/data/tasic2016/unprocessed/gtf/gencode.vM10.annotation.subset.gtf'  # noqa
+    args = 'index --sj-out-tab {splice_junctions} --gtf {gtf} --output {output}'.format(  # noqa
+        splice_junctions=sj_out_tab, gtf=gtf, output=tmpdir.strpath).split()
+    # import pdb; pdb.set_trace()
+    CommandLine(args)
+
+    dir1 = os.path.join(tmpdir.strpath, 'index')
+    dir2 = os.path.join('outrigger', 'tests', 'data', 'tasic2016',
+                        'outrigger_output', 'index')
+    ignore = ['psi', '.DS_Store', 'validated', 'splice_sites.csv',
+              # Databases get stored in a weird random way... we're still
+              # checking that the final gtfs are the same
+              'gencode.vM10.annotation.subset.gtf.db']
+    directory_comparison = filecmp.dircmp(dir1, dir2, ignore=ignore)
+
+    directory_comparison.report_full_closure()
+    assert len(directory_comparison.left_only) == 0
+    assert len(directory_comparison.right_only) == 0
+    # assert False
+
+    for subdir in directory_comparison.subdirs.values():
+        print(subdir.common_files)
+
+        assert len(subdir.left_only) == 0
+        assert len(subdir.right_only) == 0
+        for filename in subdir.common_files:
+            filename1 = os.path.join(subdir.left, filename)
+            filename2 = os.path.join(subdir.right, filename)
+
+            stat1 = os.stat(filename1)
+            stat2 = os.stat(filename2)
+            file1 =  open(filename1)
+            file2 =  open(filename2)
+
+            assert stat1.st_size == stat2.st_size
+            # for line1, line2 in zip(file1, file2):
+            #     assert line1 == line2
+
 #
 # def test_main_psi(tmpdir):
 #     from outrigger.commandline import CommandLine
