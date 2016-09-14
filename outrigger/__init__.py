75d55498a9741134127778d6c2bdfb07269ebf2b
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-09-14 10:46:01 -0700

    Version bump to v0.2.6

--- outrigger/__init__.py
+++ outrigger/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 __all__ = ['psi', 'region', 'util', 'io', 'validate', 'index',
            'common']
