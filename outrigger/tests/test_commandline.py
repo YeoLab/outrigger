357e9c59f3576de99e19082a706ef3e7900f1dc3
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-10-23 15:14:35 -0700

    Add function for checking equality of directories

--- outrigger/tests/test_commandline.py
+++ outrigger/tests/test_commandline.py
@@ -24,6 +24,57 @@ class TestSubcommand(object):
             assert os.path.exists(folder)
 
 
+def assert_directories_equal(dir1, dir2, ignore=None):
+    """Compare contents of subdirectories to assert they are equal"""
+    directory_comparison = filecmp.dircmp(dir1, dir2, ignore=ignore)
+
+    directory_comparison.report_full_closure()
+    assert len(directory_comparison.left_only) == 0
+    assert len(directory_comparison.right_only) == 0
+
+    for subdir in directory_comparison.subdirs.values():
+        print(subdir.common_files)
+
+        assert len(subdir.left_only) == 0
+        assert len(subdir.right_only) == 0
+        for filename in subdir.common_files:
+            filename1 = os.path.join(subdir.left, filename)
+            filename2 = os.path.join(subdir.right, filename)
+
+            # If the files are csv or bed tables, check that they're equal
+            df1, df2 = None, None
+
+            if filename.endswith('.csv'):
+                df1 = pd.read_csv(filename1, index_col=0)
+                df2 = pd.read_csv(filename2, index_col=0)
+
+                df1.sort_index(inplace=True)
+                df2.sort_index(inplace=True)
+
+                exons = [x for x in df1 if 'exon' in x]
+
+                if len(exons) > 0:
+                    df1.sort_values(exons, inplace=True)
+                    df2.sort_values(exons, inplace=True)
+            elif filename.endswith('.bed'):
+                df1 = pd.read_table(filename1, header=None)
+                df2 = pd.read_table(filename2, header=None)
+
+                df1.sort_values([3, 0, 1, 2], kind='mergesort', inplace=True)
+                df2.sort_values([3, 0, 1, 2], kind='mergesort', inplace=True)
+
+                df1.index = range(len(df1.index))
+                df2.index = range(len(df2.index))
+
+            if df1 is not None:
+                pdt.assert_frame_equal(df1, df2)
+
+            # Otherwise, just use the file sizes
+            stat1 = os.stat(filename1)
+            stat2 = os.stat(filename2)
+            assert stat1.st_size == stat2.st_size
+
+
 class TestCommandLine(object):
 
     def test_no_arguments(self, capsys):
@@ -93,53 +144,7 @@ class TestCommandLine(object):
                   # Databases get stored in a weird random way... we're still
                   # checking that the final gtfs are the same
                   'gencode.vM10.annotation.subset.gtf.db']
-        directory_comparison = filecmp.dircmp(dir1, dir2, ignore=ignore)
-
-        directory_comparison.report_full_closure()
-        assert len(directory_comparison.left_only) == 0
-        assert len(directory_comparison.right_only) == 0
-        # assert False
-
-        for subdir in directory_comparison.subdirs.values():
-            print(subdir.common_files)
-
-            assert len(subdir.left_only) == 0
-            assert len(subdir.right_only) == 0
-            for filename in subdir.common_files:
-                filename1 = os.path.join(subdir.left, filename)
-                filename2 = os.path.join(subdir.right, filename)
-
-                # If the files are csv or bed tables, check that they're equal
-                df1, df2 = None, None
-
-                if filename.endswith('.csv'):
-                    df1 = pd.read_csv(filename1, index_col=0)
-                    df2 = pd.read_csv(filename2, index_col=0)
-
-                    df1.sort_index(inplace=True)
-                    df2.sort_index(inplace=True)
-
-                    exons = [x for x in df1 if 'exon' in x]
-
-                    df1.sort_values(exons, inplace=True)
-                    df2.sort_values(exons, inplace=True)
-                elif filename.endswith('.bed'):
-                    df1 = pd.read_table(filename1, header=None)
-                    df2 = pd.read_table(filename2, header=None)
-
-                    df1.sort_values([3, 0, 1, 2], kind='mergesort', inplace=True)
-                    df2.sort_values([3, 0, 1, 2], kind='mergesort', inplace=True)
-
-                    df1.index = range(len(df1.index))
-                    df2.index = range(len(df2.index))
-
-                if df1 is not None:
-                    pdt.assert_frame_equal(df1, df2)
-
-                # Otherwise, just use the file sizes
-                stat1 = os.stat(filename1)
-                stat2 = os.stat(filename2)
-                assert stat1.st_size == stat2.st_size
+        assert_directories_equal(dir1, dir2, ignore)
 
     def test_main_validate(self, tmpdir, negative_control_folder,
                            negative_control_output):
@@ -153,14 +158,11 @@ class TestCommandLine(object):
                     folder=negative_control_folder),
                 '--output', tmpdir.strpath,
                 '--index', os.path.join(negative_control_output, 'index')]
-        cl = CommandLine(args)
+        CommandLine(args)
 
         dir1 = tmpdir.strpath
         dir2 = negative_control_output
-        directory_comparison = filecmp.dircmp(dir1, dir2,
-                                              ignore=['.DS_Store'])
-        assert len(directory_comparison.left_only) == 0
-        assert len(directory_comparison.right_only) == 0
+        assert_directories_equal(dir1, dir2, ignore=['.DS_Store'])
 
     def test_main_psi(self, tmpdir, tasic2016_unprocessed,
                       tasic2016_outrigger_output):
@@ -180,13 +182,9 @@ class TestCommandLine(object):
         # assert False
         CommandLine(arguments)
 
-
         args = ['psi', '--output', output_folder]
         CommandLine(args)
 
         dir1 = output_folder
         dir2 = tasic2016_outrigger_output
-        directory_comparison = filecmp.dircmp(dir1, dir2,
-                                              ignore=['.DS_Store'])
-        assert len(directory_comparison.left_only) == 0
-        assert len(directory_comparison.right_only) == 0
+        assert_directories_equal(dir1, dir2, ignore=['.DS_Store'])
