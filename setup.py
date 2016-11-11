364ed04370c8f0446c86fb44697fd509b3c497e6
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-11-11 14:09:27 -0800

    Version bump v0.2.8dev --> v0.2.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='outrigger',
-    version='0.2.8dev',
+    version='0.2.9',
     description="Outrigger is a tool to de novo annotate splice sites "
                 "and exons",
     long_description=readme + '\n\n' + history,
