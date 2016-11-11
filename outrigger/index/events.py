9520ec5c1d102cca91a653f172e720abc9d75b83
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-11-10 16:31:38 -0800

    Create SpliceGraph class as separate from the event maker

--- outrigger/index/events.py
+++ outrigger/index/events.py
@@ -27,10 +27,225 @@ def stringify_location(chrom, start, stop, strand, region=None):
 def opposite(direction):
     return UPSTREAM if direction == DOWNSTREAM else DOWNSTREAM
 
-class SpliceSubGraph(object):
 
-    def __init__(self):
-        pass
+class SpliceGraph(object):
+
+    def __init__(self, junction_exon_triples, junction_col='junction',
+                 exon_col='exon'):
+        self.junction_exon_triples = junction_exon_triples
+        self.junction_col = junction_col
+        self.exon_col = exon_col
+
+        self._make_graph(junction_exon_triples)
+
+    def _make_graph(self, junction_exon_triples):
+        self.graph = graphlite.connect(":memory:", graphs=DIRECTIONS)
+        self.exons = tuple(junction_exon_triples[self.exon_col].unique())
+        self.n_exons = len(self.exons)
+        self.junctions = tuple(
+            junction_exon_triples[self.junction_col].unique())
+
+        self.items = tuple(np.concatenate([self.exons, self.junctions]))
+        self.item_to_region = pd.Series(map(Region, self.items),
+                                        index=self.items)
+
+        with self.graph.transaction() as tr:
+            for i, row in self.junction_exon_triples.iterrows():
+                junction = row[self.junction_col]
+                exon = row[self.exon_col]
+
+                junction_i = self.items.index(junction)
+                exon_i = self.items.index(exon)
+
+                self.log.debug('\n{} is {} of {}\n'.format(
+                    exon, row.direction, junction))
+                self.log.debug('{} is {} of {}\n'.format(
+                    junction, opposite(row.direction), exon))
+
+                tr.store(getattr(V(exon_i), row.direction)(junction_i))
+                tr.store(getattr(V(junction_i),
+                                 opposite(row.direction))(exon_i))
+
+        # To speed up queries
+        self.graph.db.execute("ANALYZE upstream")
+        self.graph.db.execute("ANALYZE downstream")
+
+    def exons_one_junction_downstream(self, exon_i):
+        """Get the exon(s) that are immediately downstream of this one
+
+        Get exons that are downstream from this one, separated by one
+        junction
+
+        Parameters
+        ----------
+        exon_i : int
+            Integer identifier of the exon whose downstream exons you want.
+            This is the exon's index location in self.exon_cols
+
+        Returns
+        -------
+        downstream_exons : graphlite.Query
+            Integer identfiers of exons which are one junction downstream
+            of the provided one
+        """
+        return self.graph.find(
+                V().downstream(exon_i)).traverse(V().upstream)
+
+    def exons_one_junction_upstream(self, exon_query):
+        """Get the exon(s) that are immediately upstream of this one
+
+        Get exons that are upstream from this one, separated by one
+        junction
+
+        Parameters
+        ----------
+        exon_query : graphlite.Query
+            Integer identifier of the exon whose upstream exons you want.
+            This is the exon's index location in self.exons
+
+        Returns
+        -------
+        upstream_exons : graphlite.Query
+            Integer identfiers of exon_cols which are one junction upstream
+            of the provided one
+        """
+        return exon_query.traverse(V().downstream).traverse(
+            V().downstream)
+
+    def exons_two_junctions_downstream(self, exon_i):
+        """Get the exon(s) that are two junction hops downstream
+
+        Go one exon downstream, then one more exon. This is all the 2nd level
+        exon_cols
+
+        Parameters
+        ----------
+        exon_i : int
+            Integer identifier of the exon whose downstream exon_cols you want.
+            This is the exon's index location in self.exon_cols
+
+        Returns
+        -------
+        downstream_exons : graphlite.Query
+            Integer identfiers of exon_cols which are separated from the
+            original exon by a junction, exon, and another junction
+        """
+        return self.graph.find(V().downstream(exon_i)).traverse(
+            V().upstream).traverse(V().upstream).traverse(V().upstream)
+
+    def junctions_between_exons(self, exon_a, exon_b):
+        """Get the junctions between exonA and exonB"""
+        return self.graph.find(
+            V(exon_a).upstream) \
+            .intersection(V(exon_b).downstream)
+
+    def _skipped_exon(self, exon1_i, exon1_name):
+
+        events = {}
+
+        exon23s = list(self.exons_one_junction_downstream(exon1_i))
+        exon23s = self.item_to_region[[self.items[i] for i in exon23s]]
+
+        for exon_a, exon_b in itertools.combinations(exon23s, 2):
+            if not exon_a.overlaps(exon_b):
+                exon2 = min((exon_a, exon_b), key=lambda x: x._start)
+                exon3 = max((exon_a, exon_b), key=lambda x: x._start)
+
+                exon2_i = self.exons.index(exon2.name)
+                exon3_i = self.exons.index(exon3.name)
+
+                exon23_junction = list(self.graph.find(
+                    V(exon2_i).upstream).intersection(
+                    V().upstream(exon3_i)))
+                if len(exon23_junction) > 0:
+                    # Isoform 1 - corresponds to Psi=0. Exclusion of exon2
+                    exon13_junction = self.junctions_between_exons(
+                        exon1_i, exon3_i)
+
+                    # Isoform 2 - corresponds to Psi=1. Inclusion of exon2
+                    exon12_junction = self.junctions_between_exons(
+                        exon1_i, exon2_i)
+
+                    junctions_i = list(itertools.chain(
+                        *[exon12_junction, exon23_junction,
+                          exon13_junction]))
+                    junctions = [self.items[i] for i in junctions_i]
+                    exons = exon1_name, exon2.name, exon3.name
+
+                    events[exons] = junctions
+        return events
+
+    def _mutually_exclusive_exon(self, exon1_i, exon1_name):
+        events = {}
+
+        exon23s_from1 = self.exons_one_junction_downstream(exon1_i)
+        exon4s = self.exons_two_junctions_downstream(exon1_i)
+        exon23s_from4 = self.exons_one_junction_upstream(exon4s)
+
+        exon23s = set(exon23s_from4) & set(exon23s_from1)
+        exon23s = [self.items[i] for i in exon23s]
+
+        exon23s = self.item_to_region[exon23s]
+
+        for exon_a, exon_b in itertools.combinations(exon23s, 2):
+            if not exon_a.overlaps(exon_b):
+                exon2 = min((exon_a, exon_b), key=lambda x: x._start)
+                exon3 = max((exon_a, exon_b), key=lambda x: x._start)
+
+                exon2_i = self.items.index(exon2.name)
+                exon3_i = self.items.index(exon3.name)
+
+                exon4_from2 = set(
+                    self.exons_one_junction_downstream(exon2_i))
+                exon4_from3 = set(
+                    self.exons_one_junction_downstream(exon3_i))
+
+                exon4_is = exon4_from2 & exon4_from3
+                try:
+                    for exon4_i in exon4_is:
+                        exon4_name = self.items[exon4_i]
+                        # Isoform 1 - corresponds to Psi=0. Inclusion of
+                        # exon3
+                        exon13_junction = self.junctions_between_exons(
+                            exon1_i, exon3_i)
+
+                        exon34_junction = self.junctions_between_exons(
+                            exon3_i, exon4_i)
+
+                        # Isoform 2 - corresponds to Psi=1. Inclusion of
+                        # exon2
+                        exon12_junction = self.junctions_between_exons(
+                            exon1_i, exon2_i)
+                        exon24_junction = self.junctions_between_exons(
+                            exon2_i, exon4_i)
+
+                        exon_tuple = exon1_name, exon2.name, exon3.name, \
+                            exon4_name
+                        #             print exon12_junction.next()
+                        junctions_i = list(
+                            itertools.chain(*[exon13_junction,
+                                              exon34_junction,
+                                              exon12_junction,
+                                              exon24_junction]))
+                        junctions = [self.items[i] for i in junctions_i]
+
+                        events[exon_tuple] = junctions
+                except KeyError:
+                    pass
+
+        return events
+
+    def get_alternative_events(self, exon1_i, exon1_name):
+        events = {}
+        for event_type, event_finder in self.event_finders:
+            events[event_type].update(event_finder(exon1_i, exon1_name))
+        return events
+
+    @property
+    def event_finders(self):
+        finders = (('se', self._skipped_exon),
+                   ('mxe', self._mutually_exclusive_exon))
+        return finders
 
 
 class EventMaker(object):
@@ -60,38 +275,6 @@ class EventMaker(object):
             self.metadata, on=junction_col)
         self.db = db
 
-    def _make_graph(self, junction_exon_triples):
-
-        graph = graphlite.connect(":memory:", graphs=DIRECTIONS)
-        exons = tuple(junction_exon_triples[exon_col].unique())
-        self.n_exons = len(self.exons)
-        self.junctions = tuple(junction_exon_triples[junction_col].unique())
-
-        self.items = tuple(np.concatenate([self.exons, self.junctions]))
-        self.item_to_region = pd.Series(map(Region, self.items),
-                                        index=self.items)
-        import pdb; pdb.set_trace()
-
-        with self.graph.transaction() as tr:
-            for i, row in self.junction_exon_triples.iterrows():
-                junction = row[junction_col]
-                exon = row[exon_col]
-
-                junction_i = self.items.index(junction)
-                exon_i = self.items.index(exon)
-
-                self.log.debug('\n{} is {} of {}\n'.format(
-                    exon, row.direction, junction))
-                self.log.debug('{} is {} of {}\n'.format(
-                    junction, opposite(row.direction), exon))
-
-                tr.store(getattr(V(exon_i), row.direction)(junction_i))
-                tr.store(getattr(V(junction_i),
-                                 opposite(row.direction))(exon_i))
-
-        # To speed up queries
-        self.graph.db.execute("ANALYZE upstream")
-        self.graph.db.execute("ANALYZE downstream")
 
     @property
     def exon_progress_interval(self):
@@ -189,75 +372,6 @@ class EventMaker(object):
             events[ILLEGAL_JUNCTIONS] = illegal_junctions
         return events
 
-    def exons_one_junction_downstream(self, exon_i):
-        """Get the exon(s) that are immediately downstream of this one
-
-        Get exons that are downstream from this one, separated by one
-        junction
-
-        Parameters
-        ----------
-        exon_i : int
-            Integer identifier of the exon whose downstream exons you want.
-            This is the exon's index location in self.exon_cols
-
-        Returns
-        -------
-        downstream_exons : graphlite.Query
-            Integer identfiers of exons which are one junction downstream
-            of the provided one
-        """
-        return self.graph.find(
-                V().downstream(exon_i)).traverse(V().upstream)
-
-    def exons_one_junction_upstream(self, exon_query):
-        """Get the exon(s) that are immediately upstream of this one
-
-        Get exons that are upstream from this one, separated by one
-        junction
-
-        Parameters
-        ----------
-        exon_query : graphlite.Query
-            Integer identifier of the exon whose upstream exons you want.
-            This is the exon's index location in self.exons
-
-        Returns
-        -------
-        upstream_exons : graphlite.Query
-            Integer identfiers of exon_cols which are one junction upstream
-            of the provided one
-        """
-        return exon_query.traverse(V().downstream).traverse(
-            V().downstream)
-
-    def exons_two_junctions_downstream(self, exon_i):
-        """Get the exon(s) that are two junction hops downstream
-
-        Go one exon downstream, then one more exon. This is all the 2nd level
-        exon_cols
-
-        Parameters
-        ----------
-        exon_i : int
-            Integer identifier of the exon whose downstream exon_cols you want.
-            This is the exon's index location in self.exon_cols
-
-        Returns
-        -------
-        downstream_exons : graphlite.Query
-            Integer identfiers of exon_cols which are separated from the
-            original exon by a junction, exon, and another junction
-        """
-        return self.graph.find(V().downstream(exon_i)).traverse(
-            V().upstream).traverse(V().upstream).traverse(V().upstream)
-
-    def junctions_between_exons(self, exon_a, exon_b):
-        """Get the junctions between exonA and exonB"""
-        return self.graph.find(
-            V(exon_a).upstream) \
-            .intersection(V(exon_b).downstream)
-
     def find_events(self, n_jobs=-1):
         """Iterate over all exons and test if they could be part of an alternative event"""
         events = dict.fromkeys(SPLICE_ABBREVS)
@@ -280,110 +394,5 @@ class EventMaker(object):
             events_dfs[event_type] = events_df
         return events_dfs
 
-    def get_alternative_events(self, exon1_i, exon1_name):
-        events = {}
-        for event_type, event_finder in self.event_finders:
-            events[event_type].update(event_finder(exon1_i, exon1_name))
-        return events
 
-    @property
-    def event_finders(self):
-        finders = (('se', self._skipped_exon),
-                   ('mxe', self._mutually_exclusive_exon))
-        return finders
 
-    def _skipped_exon(self, exon1_i, exon1_name):
-
-        events = {}
-
-        exon23s = list(self.exons_one_junction_downstream(exon1_i))
-        exon23s = self.item_to_region[[self.items[i] for i in exon23s]]
-
-        for exon_a, exon_b in itertools.combinations(exon23s, 2):
-            if not exon_a.overlaps(exon_b):
-                exon2 = min((exon_a, exon_b), key=lambda x: x._start)
-                exon3 = max((exon_a, exon_b), key=lambda x: x._start)
-
-                exon2_i = self.exons.index(exon2.name)
-                exon3_i = self.exons.index(exon3.name)
-
-                exon23_junction = list(self.graph.find(
-                    V(exon2_i).upstream).intersection(
-                    V().upstream(exon3_i)))
-                if len(exon23_junction) > 0:
-                    # Isoform 1 - corresponds to Psi=0. Exclusion of exon2
-                    exon13_junction = self.junctions_between_exons(
-                        exon1_i, exon3_i)
-
-                    # Isoform 2 - corresponds to Psi=1. Inclusion of exon2
-                    exon12_junction = self.junctions_between_exons(
-                        exon1_i, exon2_i)
-
-                    junctions_i = list(itertools.chain(
-                        *[exon12_junction, exon23_junction,
-                          exon13_junction]))
-                    junctions = [self.items[i] for i in junctions_i]
-                    exons = exon1_name, exon2.name, exon3.name
-
-                    events[exons] = junctions
-        return events
-
-    def _mutually_exclusive_exon(self, exon1_i, exon1_name):
-        events = {}
-
-        exon23s_from1 = self.exons_one_junction_downstream(exon1_i)
-        exon4s = self.exons_two_junctions_downstream(exon1_i)
-        exon23s_from4 = self.exons_one_junction_upstream(exon4s)
-
-        exon23s = set(exon23s_from4) & set(exon23s_from1)
-        exon23s = [self.items[i] for i in exon23s]
-
-        exon23s = self.item_to_region[exon23s]
-
-        for exon_a, exon_b in itertools.combinations(exon23s, 2):
-            if not exon_a.overlaps(exon_b):
-                exon2 = min((exon_a, exon_b), key=lambda x: x._start)
-                exon3 = max((exon_a, exon_b), key=lambda x: x._start)
-
-                exon2_i = self.items.index(exon2.name)
-                exon3_i = self.items.index(exon3.name)
-
-                exon4_from2 = set(
-                    self.exons_one_junction_downstream(exon2_i))
-                exon4_from3 = set(
-                    self.exons_one_junction_downstream(exon3_i))
-
-                exon4_is = exon4_from2 & exon4_from3
-                try:
-                    for exon4_i in exon4_is:
-                        exon4_name = self.items[exon4_i]
-                        # Isoform 1 - corresponds to Psi=0. Inclusion of
-                        # exon3
-                        exon13_junction = self.junctions_between_exons(
-                            exon1_i, exon3_i)
-
-                        exon34_junction = self.junctions_between_exons(
-                            exon3_i, exon4_i)
-
-                        # Isoform 2 - corresponds to Psi=1. Inclusion of
-                        # exon2
-                        exon12_junction = self.junctions_between_exons(
-                            exon1_i, exon2_i)
-                        exon24_junction = self.junctions_between_exons(
-                            exon2_i, exon4_i)
-
-                        exon_tuple = exon1_name, exon2.name, exon3.name, \
-                            exon4_name
-                        #             print exon12_junction.next()
-                        junctions_i = list(
-                            itertools.chain(*[exon13_junction,
-                                              exon34_junction,
-                                              exon12_junction,
-                                              exon24_junction]))
-                        junctions = [self.items[i] for i in junctions_i]
-
-                        events[exon_tuple] = junctions
-                except KeyError:
-                    pass
-
-        return events
