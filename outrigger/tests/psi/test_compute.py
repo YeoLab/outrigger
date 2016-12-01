d0b2b6e1ed12b8ab5b758fa2968594c6ba9c05b5
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-12-01 12:13:09 -0800

    Fillna with 0 for test read data

--- outrigger/tests/psi/test_compute.py
+++ outrigger/tests/psi/test_compute.py
@@ -322,6 +322,7 @@ def reads_2d(splice_junction_reads_csv):
     from outrigger.common import SAMPLE_ID, JUNCTION_ID, READS
     df = pd.read_csv(splice_junction_reads_csv)
     reads2d = df.pivot(index=SAMPLE_ID, columns=JUNCTION_ID, values=READS)
+    reads2d = reads2d.fillna(0)
     return reads2d
 
 
