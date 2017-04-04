fe012115f3ebb14eb3cf6313aa24fd4d3668bc1c
Olga Botvinnik <olga.botvinnik@gmail.com>
2017-04-03 17:29:59 -0700

    Specify Python 3.6 compatibility for PyPI and PR template

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     tests_require=test_requirements,
     entry_points={
